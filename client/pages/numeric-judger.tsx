73cdbca48d18784592c0fda0e34e13374cacbec9
osawa-koki <github@osawa.dev>
2023-01-03 21:56:57 +0900

    画像データ送信処理。

--- client/pages/numeric-judger.tsx
+++ client/pages/numeric-judger.tsx
@@ -10,15 +10,47 @@ function MyDrawing() {
     canvas.remove.apply(canvas, canvas.getObjects())
   }
 
+  const SendBinary = (e) => {
+    // 画像をバイナリに変換
+    // create a new canvas element to store the data
+    const dataCanvas = document.createElement('canvas');
+    dataCanvas.width = canvas.getWidth();
+    dataCanvas.height = canvas.getHeight();
+
+    // copy the data from the original canvas to the new canvas
+    const ctx = dataCanvas.getContext('2d');
+    ctx.drawImage(canvas.getElement(), 0, 0);
+
+    // get the data from the new canvas as a binary data
+    const data = dataCanvas.toDataURL('image/png');
+    const binaryData = atob(data.split(',')[1]);
+    var buffer = new Uint8Array(binaryData.length);
+    for (var i = 0; i < binaryData.length; i++) {
+      buffer[i] = binaryData.charCodeAt(i);
+    }
+    // Blobを作成
+    var blob = new Blob([buffer.buffer]);
+    // BlobをFormDataに追加
+    var formData = new FormData();
+    formData.append('image', blob);
+    // FormDataをPOST
+    fetch('/api/numeric-judger', {
+      method: 'POST',
+      body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
+  };
+
   useEffect(() => {
     if (canvas === null) return;
     canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
     canvas.freeDrawingBrush.width=5;
     canvas.freeDrawingBrush.color="black";
     canvas.isDrawingMode = true;
-    canvas.on('path:created', function (e) {
-      console.log('path:created');
-    })
+    canvas.on('path:created', SendBinary);
   }, [canvas]);
 
   useEffect(() => {
