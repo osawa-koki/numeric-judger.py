e56d56cec32a107a1d740eec4697d62268a2d641
osawa-koki <github@osawa.dev>
2023-01-03 22:13:49 +0900

    CORSとファイ名変えた。

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from fastapi import FastAPI, File, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
+import uuid
 
 app = FastAPI()
 
+# CORSを許可する
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.post("/api/numeric-judge")
 def receive_image(image: bytes = File(...)):
-    # 現在の日時を取得
-    now = datetime.now()
-    # 日時をフォーマットする
-    filename = now.strftime("%Y%m%d%H%M%S")
+    # GUIDを生成する
+    guid = uuid.uuid4()
+    # GUIDを小文字に変換する
+    guid = guid.hex.lower()
+
     # 画像を保存する
-    with open(filename, "wb") as f:
+    with open(f"{guid}.png", "wb") as f:
         f.write(image)
     return {"received": True}
 
